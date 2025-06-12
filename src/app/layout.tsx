3a0bd875f7c11f461d84ed264dda4a2937ec5ff0
Gyeongtae Gim <elnotlu@gmail.com>
2025-06-12 14:51:11 +0900

    feat: header 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 import './globals.css';
+import Header from './header';
 
 export const metadata: Metadata = {
   title: 'Create Next Blog',
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
